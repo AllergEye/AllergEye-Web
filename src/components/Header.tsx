4d7854a65b1f12baa4908e0ae539880ebc3d8118
Reezan Visram <rhvisram@uwaterloo.ca>
2023-08-19 13:36:00 -0400

    create initial next poc

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+import './Header.css';
+export default function Header() {
+    return (
+        <nav className="bg-cerulean flex drop-shadow-md">
+            <div>
+                <h2 className="text-3xl m-5 text-white">AllergEye</h2>
+            </div>
+        </nav>
+    );
+}
